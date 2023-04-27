c47c706fc5581f46b151c63c1a7a78af9cb75717
JayBrown92 <49319765+JayBrown92@users.noreply.github.com>
2023-04-27 01:42:08 +0100

    added function to map through template icons instead of repeating jsx

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -8,23 +8,45 @@ type PropTypes = {
   children: React.ReactNode;
 };
 
+type IconTypes = {
+  src: any;
+  alt: string;
+};
+
+const icons: IconTypes[] = [
+  {
+    src: GithubIcon,
+    alt: "github icon",
+  },
+  {
+    src: FrontendMentorIcon,
+    alt: "Frontend Mentor icon",
+  },
+  {
+    src: LinkedInIcon,
+    alt: "LinkedIn icon",
+  },
+  {
+    src: TwitterIcon,
+    alt: "Twitter icon",
+  },
+];
 export default function Template({ children }: PropTypes) {
+  const logo = <h4 className="basis-full text-center">adamkeyes</h4>;
+  const iconMap = icons.map((icon, index) => {
+    const { src, alt } = icon;
+    return <Image key={index} src={src} alt={alt} width={20} height={20} />;
+  });
   return (
     <>
       <header className=" flex flex-wrap justify-center gap-6 py-5">
-        <h4 className="basis-full text-center">adamkeyes</h4>
-        <Image src={GithubIcon} width={20} alt="github icon" />
-        <Image src={FrontendMentorIcon} width={20} alt="Frontend Mentor icon" />
-        <Image src={LinkedInIcon} width={20} alt="LinkedIn icon" />
-        <Image src={TwitterIcon} width={20} alt="Twitter icon" />{" "}
+        {logo}
+        {iconMap}
       </header>
       {children}
       <footer className=" flex flex-wrap justify-center gap-6 py-10">
-        <h4 className="basis-full text-center">adamkeyes</h4>
-        <Image src={GithubIcon} width={20} alt="github icon" />
-        <Image src={FrontendMentorIcon} width={20} alt="Frontend Mentor icon" />
-        <Image src={LinkedInIcon} width={20} alt="LinkedIn icon" />
-        <Image src={TwitterIcon} width={20} alt="Twitter icon" />{" "}
+        {logo}
+        {iconMap}
       </footer>
     </>
   );
