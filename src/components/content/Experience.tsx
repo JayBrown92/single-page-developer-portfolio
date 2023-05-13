66eb33e31097c2255be31dd87ded8dc374814ead
JayBrown92 <49319765+JayBrown92@users.noreply.github.com>
2023-05-13 22:23:50 +0100

    complete: experience sec and moved rings pattern out of index

--- src/components/content/Experience.tsx
+++ src/components/content/Experience.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import ringsPattern from "../../../public/pattern-rings.svg";
+
 import Section from "../UI/Section";
 import Container from "../UI/Container";
 
@@ -32,7 +34,7 @@ export default function Experience() {
 
   return (
     <Section id={"experience-section"}>
-      <Container className="  grid gap-6  py-10 text-center  before:absolute before:left-4 before:right-4 before:top-0 before:mx-auto before:border-y-[1px] before:border-color-primary  after:absolute after:bottom-0 after:left-4 after:right-4 after:mx-auto after:border-y-[1px] after:border-color-primary md:grid-cols-2 md:py-14 md:text-left md:after:hidden">
+      <Container className="  grid gap-6 overflow-visible py-10 text-center  before:absolute before:left-4 before:right-4 before:top-0 before:mx-auto before:border-y-[1px] before:border-color-primary  after:absolute after:bottom-0 after:left-4 after:right-4 after:mx-auto after:border-y-[1px] after:border-color-primary md:grid-cols-2 md:py-14 md:text-left md:before:left-8 md:before:right-8 md:after:hidden lg:grid-cols-3 lg:before:left-16 lg:before:right-16">
         {ExpContent.map((content, index) => {
           const { header, text } = content;
           return (
@@ -42,6 +44,13 @@ export default function Experience() {
             </div>
           );
         })}
+        <Image
+          src={ringsPattern}
+          width={530}
+          height={129}
+          alt="pattern for hero section design consiting of multiple flat rings"
+          className="absolute -bottom-16 z-10 -translate-y-1/2 md:-right-96 "
+        />
       </Container>
     </Section>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import ringsPattern from "../../public/pattern-rings.svg";
-
 import Hero from "../components/content/Hero";
 import Experience from "../components/content/Experience";
 import Projects from "../components/content/Projects";
@@ -10,13 +7,7 @@ export default function Home() {
     <>
       <Hero />
       <Experience />
-      <Image
-        src={ringsPattern}
-        width={530}
-        height={129}
-        alt="pattern for hero section design consiting of multiple flat rings"
-        className="absolute -right-48 z-10 -translate-y-1/2 md:-right-96 "
-      />
+
       <Projects />
       <Contact />
     </>
